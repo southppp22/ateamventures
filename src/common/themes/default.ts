cf68cb7ad6a509ceda7d248c503a226d3fa9466d
southppp22 <tnddl032@gmail.com>
2021-09-12 17:57:16 +0900

    add common style

---
+++ src/common/themes/default.ts
@@ -0,0 +1,49 @@
+const theme = {
+  palette: {
+    black: `#000`,
+    white: `#fff`,
+    lightGray: `#E5E5E5`,
+    gray: `#323D45`,
+    subGray: `#939FA5`,
+    primary: `#2196F3`,
+    primaryDark: `#1565C0`,
+    warningDark: `#FFA000`,
+  },
+  fontStyle: {
+    body1: `
+      font-size: 2rem;
+      line-height: 3.2rem
+      font-weight: 700;
+    `,
+    body2: `
+      font-size: 1.6rem;
+      font-weight: 400;
+      line-height: 2.4rem;
+    `,
+    body3: `
+      font-size: 1.4rem;
+      line-height: 2rem;
+      font-weight: 500;
+    `,
+    body4: `
+      font-size: 1.4rem;
+      line-height: 2rem;
+      font-weight: 400;
+    `,
+    body5: `
+      font-size: 1.2rem;
+      line-height: 1.4rem;
+      font-weight: 500
+    `,
+  },
+  btnStyle: {
+    default: `
+      padding: 0.6rem 1.2rem;
+      border-radius: 0.4rem;
+      border-style: solid;
+      border-width: thin;
+    `,
+  },
+};
+
+export default theme;
