8801edcbc112ccc4238c962794ed82071cfa3b2f
southppp22 <tnddl032@gmail.com>
2021-09-12 18:03:10 +0900

    add dummy

---
+++ src/fixtures/filterOptions.ts
@@ -0,0 +1,45 @@
+import { FilterOptions } from "types/Data";
+
+const filterOptions: FilterOptions = {
+  method: [
+    {
+      id: 1,
+      name: "밀링",
+      status: false,
+    },
+    {
+      id: 2,
+      name: "선반",
+      status: false,
+    },
+  ],
+  material: [
+    {
+      id: 1,
+      name: "알루미늄",
+      status: false,
+    },
+    {
+      id: 2,
+      name: "탄소강",
+      status: false,
+    },
+    {
+      id: 3,
+      name: "구리",
+      status: false,
+    },
+    {
+      id: 4,
+      name: "합금강",
+      status: false,
+    },
+    {
+      id: 5,
+      name: "강철",
+      status: false,
+    },
+  ],
+};
+
+export default filterOptions;
